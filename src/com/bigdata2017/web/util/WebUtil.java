039e0da343b8e193e7f9a6f70bb6636fd1cd0c05
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-08-28 10:47:47 +0900

    webutil 수정

--- src/com/bigdata2017/web/util/WebUtil.java
+++ src/com/bigdata2017/web/util/WebUtil.java
@@ -18,8 +18,13 @@ public class WebUtil {
 		rd.forward( request, response );
 	}
 	
-	public static void redirect(String url, HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public static void redirect(
+		String url, 
+		HttpServletRequest request, 
+		HttpServletResponse response)
+		throws ServletException, IOException {
+		
 		response.sendRedirect( url );
+	
 	}	
 }
